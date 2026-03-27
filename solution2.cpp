4560cf83ff52c0afd31261331f4ca1f703eb8707
Vennela Varshini Anasoori <vennelavarshini07@gmail.com>
2026-03-27 22:09:49 +0530

    basis server cost calculation

---
+++ solution2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    long long n, m, a, b;
+    cin >> n >> m >> a >> b;
+    
+    long long cost = n * a;
+    
+    cout << cost << endl;
+    return 0;
+}
