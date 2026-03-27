7d43a9a70ebdd82b311c04977c754327c20e72b1
Vennela Varshini Anasoori <vennelavarshini07@gmail.com>
2026-03-27 22:11:23 +0530

    add bundle overbuy logic

--- solution2.cpp
+++ solution2.cpp
@@ -6,7 +6,7 @@ int main() {
     long long n, m, a, b;
     cin >> n >> m >> a >> b;
     
-    long long cost = n * a;
+    long long cost = min(n * a, ((n + m - 1) / m) * b);
     
     cout << cost << endl;
     return 0;
